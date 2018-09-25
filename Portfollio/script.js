acbc72cade48cf456a5081399c70d80d6a92b4e6
Zion Dalley <43570942+ZionDalley@users.noreply.github.com>
2018-09-25 06:58:02 -0400

    Initial Commit

---
+++ Portfollio/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#fullpage").fullpage({
+        autoScrolling: true,
+        scrollBar: true,
+        keyboardScrolling: true,
+        lazyLoading: true,
+        dragAndMove: true,
+        fitToSectionDelay: 500,
+        scrollingSpeed: 750,
+        scrollOverflow: false,
+
+    });
+})
